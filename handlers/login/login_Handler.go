2f4e94046198513fe571760b709d5592f8206d49
Rafael Porto <rafaelportot@proton.me>
2024-10-21 20:19:56 +0000

    Adicionado handlelogin

---
+++ handlers/login/login_Handler.go
@@ -0,0 +1,7 @@
+package login_handlers
+
+import "net/http"
+
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	login "go-chat/handlers/login"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupRouters() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/login", login.HandleLogin)
+}
