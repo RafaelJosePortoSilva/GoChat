b0d1939c330a8d1d3aa82da9daa8771df1ea97e1
Rafael Porto <rafaelportot@proton.me>
2024-10-22 13:59:15 +0000

    Adicionado service de login com hash e um repo com retorno default

--- repositories/login/login_repositories.go
+++ repositories/login/login_repositories.go
@@ -1,9 +1,21 @@
 package login_repositories
 
-func GetPasswordByUsername(username string) string {
-	return "pass"
+func GetPasswordByUsername(username string) (string, error) {
+	return "pass", nil
+}
+
+func CreateNewLogin(username string, hash string) error {
+	if !verifyUsernameDuplicity(username) {
+		return nil
+	} else {
+		return nil // nao lembro como retornar erro
+	}
 }
 
 func verifyUsernameDuplicity(username string) bool {
 	return false
+}
+
+func VerifyUsernameExists(username string) error {
+	return nil
 }
--- services/login/login_services.go
+++ services/login/login_services.go
@@ -1,10 +1,31 @@
 package services_login
 
 import (
+	"fmt"
+	login_repo "go-chat/repositories/login"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
-func AuthUser(username string, pass string) {
+func AuthUser(username string, pass string) error {
+
+	err := login_repo.VerifyUsernameExists(username)
+	if err != nil {
+		return fmt.Errorf("invalid username")
+	}
+
+	hashPassInDB, err := login_repo.GetPasswordByUsername(username)
+	if err != nil {
+		return fmt.Errorf("internal error")
+	}
+
+	isHashCorrect := checkPasswordHash(pass, hashPassInDB)
+
+	if isHashCorrect {
+		return nil
+	} else {
+		return fmt.Errorf("invalid password")
+	}
 
 }
 
