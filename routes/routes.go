ba0f199a0c8b34b4433264af564591f7cc2e4b08
Rafael Porto <rafaelportot@proton.me>
2024-10-22 20:53:30 +0000

    Feito parte do front de login

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	login_handlers "go-chat/handlers/login"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -12,7 +13,12 @@ func SetupRouters() *mux.Router {
 	chat := r.PathPrefix("/chat").Subrouter()
 	chat.Use(login_handlers.AuthMiddleware)
 
-	r.HandleFunc("/login/auth", login_handlers.HandleLogin)
+	login := r.PathPrefix("/login").Subrouter()
+
+	login.HandleFunc("/auth", login_handlers.HandleLogin)
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/login.html")
+	})
 
 	return r
 }
---
+++ static/login/login.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("/login/auth", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.json();
+            document.getElementById("error-message").innerText = errorMessage.message;
+        }
+    } catch (error) {
+        console.error("Error logging in:", error);
+        document.getElementById("error-message").innerText = "An error occurred during login.";
+    }
+});
